73c551d5d3b03356039d49735d6699dc64125b62
Ahmed <gfg59204@gmail.com>
2024-07-19 07:12:28 +0400

    add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { LandingPage } from "@/components/component/landing-page";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -21,7 +22,7 @@ export default async function Index() {
 
   return (
    <>
-   
+   <LandingPage/>
    
    </>
   );
